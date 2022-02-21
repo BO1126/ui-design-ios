bb949baaa97b823dd1d340efa3d689dffc4baf7b
jungwoo Lee <b5nnnal88@gmail.com>
2022-02-22 01:25:35 +0900

    Start first my UI design

---
+++ My UI Design/My UI Design/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My UI Design
+//
+//  Created by 이정우 on 2022/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
